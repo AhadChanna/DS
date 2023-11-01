71c8593d885e4bd0a9db970633e53836c7256870
mujeeb <datastructure@gmail.com>
2023-11-01 11:33:30 +0500

    square is completed

--- Shapes.java
+++ Shapes.java
@@ -3,6 +3,9 @@ public class Shapes{
     public void circle(){
         System.out.println("circle is made");
     }
+     public void square(){
+        System.out.println("square is made");
+    }
 
     public static void main (string args []){
 
